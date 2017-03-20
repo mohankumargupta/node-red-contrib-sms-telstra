a8996a97f3e8a529fe013086ef494ea78a1f0907
mohankumargupta <mohankumargupta@users.noreply.github.com>
2017-03-20 16:03:19 +1100

    Update smstelstra.js

--- smstelstra.js
+++ smstelstra.js
@@ -9,7 +9,7 @@ module.exports = function (RED) {
     var messagepayload = "";
 
     this.on('input', function(msg) {
-      node.status({fill: 'green', shape: 'dot', text: " "});
+      node.status({fill: 'green', shape: 'dot', text: " "});      
       var options = {
         url: 'https://api.telstra.com/v1/oauth/token',
         method: 'POST',
@@ -36,40 +36,21 @@ module.exports = function (RED) {
          }          
         }
         
-                       
-        var options2 = {
-          url: 'https://api.telstra.com/v1/sms/messages',
-          method: 'POST',
-          headers: {
-            Authorization: authorization  
-          },
-          body: {
-            to: config.mobile,
-            body: message
-          },
-          json: true
-        };
-        options2.headers['Content-Type'] = 'application/json';
-        httpsrequest(options2).then(function(body2) {
-         node.status({});
-         msg.payload = 'SmsTelstra:Success';
-         node.send(msg);
-        }).catch( errors.StatusCodeError, function(reason){
-         node.status({fill: 'red', shape: 'dot', text: " "});
-         setTimeout(function() {
-         node.status({});
-       }, 3000);
-         node.error(reason);
-       }).catch( errors.RequestError, function(reason){
-         node.status({fill: 'red', shape: 'dot', text: " "});
-         setTimeout(function() {
-         node.status({});
-       }, 3000);
-         node.error(reason);
-       });
+        var mobilenumbers = config.mobile.replace(/\s+/g, '').split(",");             
+ 
+        mobilenumbers.forEach(function(mobile) {
+          setTimeout(function() {
+            sendSMS(mobile,message, authorization, node, msg);
+          },5000);
+        });
 
 
 
+        setTimeout(function() {
+          node.status({});
+          msg.payload = "SMSTelstra: Success";
+          node.send(msg);
+        }, 5000);
       }).catch(errors.StatusCodeError, function(reason) {
        node.status({fill: 'red', shape: 'dot', text: " "});
        setTimeout(function() {
@@ -84,10 +65,44 @@ module.exports = function (RED) {
          node.error(reason);
 
         });
-    });
+     
+   });
+  }   
 
+  function sendSMS(mobilenumber, message, authorization, node, msg) {
+       node.status({fill: 'green', shape: 'dot', text: " "});
+       var options2 = {
+          url: 'https://api.telstra.com/v1/sms/messages',
+          method: 'POST',
+          headers: {
+            Authorization: authorization  
+          },
+          body: {
+            to: mobilenumber,
+            body: message
+          },
+          json: true
+        };
+        options2.headers['Content-Type'] = 'application/json';
+        httpsrequest(options2).then(function(body2) {
+         
+        
+        }).catch( errors.StatusCodeError, function(reason){
+         node.status({fill: 'red', shape: 'dot', text: " "});
+         setTimeout(function() {
+         node.status({});
+         }, 3000);
+         node.error(reason);
+       }).catch( errors.RequestError, function(reason){
+         node.status({fill: 'red', shape: 'dot', text: " "});
+         setTimeout(function() {
+         node.status({});
+       }, 3000);
+         node.error(reason);
+       });
   }
-  
+
   RED.nodes.registerType("smstelstra", SmsTelstraNode);
 
 };
+
